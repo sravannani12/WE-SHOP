426a947191963eb478ae7c09b786af0366c4038f
sravannani12 <nanisravan89@gmail.com>
2021-11-07 18:41:17 +0530

    Created Rating Component Page

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import { Card } from 'react-bootstrap';
+import Rating from '../components/Rating';
 
 const Product = ({product}) => {
     return (
-        <Card className="my-3 p-3 rounded" >
+        <Card>
             <a href={`/product/${product._id}`}>
-                <Card.Img src={product.image} variant='top'/>
+                <Card.Img src={product.image} />
             </a>
             <Card.Body>
                 <a href={`/product/${product._id}`}>
                     <Card.Title as='div'>{product.name}</Card.Title>
                 </a>
-                <Card.Text as='div'>
-                    <div className='my-3'>
-                        {product.rating} from {product.numReviews} reviews
-                    </div>
-                </Card.Text>
-                <Card.Text as='h3'>${product.price}</Card.Text>
+                <Card.Text as ='div'>
+                    <Rating 
+                    value={product.rating} text={`${product.numReviews} reviews`}
+                    />
+                </Card.Text> 
+                <Card.Text as ='h3'>${product.price}</Card.Text>
             </Card.Body>
         </Card>
     )
